c1db538445b1b6ea12fcbffec4953cfcef93bc4f
lnwchok <sitthichok.punlabpho@gmail.com>
2025-07-07 23:44:04 +0700

    Update Controller

--- controller/componentController.js
+++ controller/componentController.js
@@ -1,16 +1,18 @@
 const db = require('../models/db');
 
+// curl http://localhost:3001/api/components/
 const getComps = async (req, res) => {
     db.all('SELECT * FROM pipes', (err, rows) => {
     if (err) {
       console.error(err.message);
       res.status(500).json({ message: 'Error retrieving components', error: error.message });
     } else {
-      res.status(200).json(rows);
+      res.status(200).json({ nps : 'All', data : rows});
     }
   });
 };
 
+// curl http://localhost:3001/api/components/1_1_2
 const getCompbyNPS = (req, res) => {
  const { nps } = req.params;
   db.get('SELECT * FROM pipes WHERE nps = ?', [nps], (err, row) => {
@@ -20,7 +22,7 @@ const getCompbyNPS = (req, res) => {
     } else if (!row) {
       res.status(404).json({ message: 'The NPS not found' });
     } else {
-      res.status(200).json(row);
+      res.status(200).json(data = { nps : row.nps, data : row});
     }
   }); 
 }
